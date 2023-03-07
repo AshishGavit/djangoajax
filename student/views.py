20ec79caad14df139029cfe8bfde3ccdb606a59b
Ashish Gavit <ashishgavit2009@gmail.com>
2023-03-07 17:27:19 +0530

    Updated home() function and to get list of students
    Imported the Student model and created and queryset of students by using all() method.
    Then render the object 'students' to home.html page and display the list of student in a table.

--- student/views.py
+++ student/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Student
 from .forms import StudentForm
 
 def home(request):
     form = StudentForm()
+    students = Student.objects.all() 
     context = {
         'form':form,
+        'students':students,
     }
     return render(request, 'home.html', context)
