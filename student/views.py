1f9e85404c22566d709b8f8a4a36c958de0d12e3
Ashish Gavit <ashishgavit2009@gmail.com>
2023-03-10 14:35:06 +0530

    Updated urls.py and views.py files     urls.py:         - A new url path is included for saveing the data     views.py:         - A new function name 'save_data()' is added.         - This function receives a 'POST' requests, validate, and save the data to database.         - It send a list of student data using JasonResponse() back to the ajax on success.

--- student/views.py
+++ student/views.py
@@ -1,6 +1,9 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
 from .models import Student
 from .forms import StudentForm
+
 
 def home(request):
     form = StudentForm()
@@ -10,3 +13,24 @@ def home(request):
         'students':students,
     }
     return render(request, 'home.html', context)
+
+@csrf_protect
+def save_data(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            # Save the data to the database
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            phone = form.cleaned_data['phone']
+            student = Student(name=name, email=email, phone=phone)
+            student.save()
+
+            # Retrieve all the data from the database
+            queryset = Student.objects.values()
+            students = list(queryset)
+            return JsonResponse({'status': 'success', 'message': 'Data saved successfully!', 'students':students})
+        else:
+            return JsonResponse({'status':'failure', 'message':'Unable to save the data'})
+    else:
+        return JsonResponse({'status':'failure', 'message':'Unable to save the data'})
