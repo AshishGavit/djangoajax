4bf25e5a3dc1fe91b37312296f31322815717172
Ashish Gavit <ashishgavit2009@gmail.com>
2023-03-25 15:36:35 +0530

    Improved validation and added update feature.
    Refactored code to implement class-based validation error handling and added a cancel button for save/update functionality.
    Additionally, added jQuery functionality to change the save button to an update button on edit button click and introduced a new 'btnupdate' button for handling updates.

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -46,25 +46,17 @@ $(document).ready(function() {
         },
         errorPlacement: function(error, element) {
             // display error message in red
-            error.css({
-                color: 'red',
-            }); 
+            error.addClass('error');
             error.insertAfter(element);
 
             // change field border on error
-            element.css({
-                borderColor: 'red',
-            });
+            $(element).addClass('error');
         },
         success: function(label, element) {
-            $(element).css('border-color', 'green');
-            // remove border color on success
-            setTimeout(function() {
-              $(element).css('border-color', '');
-            }, 3000); // 3 seconds
-        }
+            $(element).removeClass('error');
+        },
     });
-    $('#btnsave').on('click', function(event){
+    $('#formid').on('click', '#btnsave', function(event){
         event.preventDefault();
         if($('#formid').valid()){
             // submit form data to the function save_data() in views.py using ajax
@@ -86,6 +78,7 @@ $(document).ready(function() {
                         }
                         $('#tbody').html(output);
                         $('form')[0].reset();
+                        $('#formid').validate().resetForm();
                     }
                     else{
                         console.log(data.message)
@@ -96,6 +89,8 @@ $(document).ready(function() {
             console.log('Form is invalid');
         }
     });
+    
+    // Bind click event handler to parent element
     $('#tbody').on('click', '.btn-delete', function(){
         rowID = $(this).data('sid')
         csrfToken = $('input[name="csrfmiddlewaretoken"]').val()
@@ -110,6 +105,11 @@ $(document).ready(function() {
             success: function(response){
                 // remove the row from table
                 $('#' + rowID).remove();
+                $('form')[0].reset();
+                $('#btnupdate').off('click');
+                $('#btnupdate').attr({'value': 'Save', 'id':'btnsave'}).removeClass('btn-warning').addClass('btn-success');
+                $('#form-title').text('Add Student').removeClass('alert-warning').addClass('alert-info');
+                $("#formid").validate().resetForm();
             },
             error: function(xhr, status, error){
                 // handle error response
@@ -117,7 +117,10 @@ $(document).ready(function() {
             }
         });
     });
+
+    // Bind click event handler to parent element
     $('#tbody').on('click', '.btn-edit', function(){
+        
         rowID = $(this).data('sid');
         csrfToken = $('input[name="csrfmiddlewaretoken"]').val()
         $.ajax({
@@ -125,11 +128,66 @@ $(document).ready(function() {
             method: 'POST',
             data: {'row_id':rowID, 'csrfmiddlewaretoken':csrfToken,},
             success: function(response){
+                // reset the form
+                $("#formid").validate().resetForm();
+
+                // display the data in respective form fields
                 $('#studentid').val(response.id)
                 $('#nameid').val(response.name)
                 $('#emailid').val(response.email)
                 $('#phoneid').val(response.phone)
+
+                // change the heading as 'Update Student' and also change the functionality of save button to update button
+                $('#btnsave').off('click');
+                $('#btnsave').attr({'value': 'Update', 'id':'btnupdate'}).removeClass('btn-success').addClass('btn-warning');
+                $('#form-title').text('Update Student').removeClass('alert-info').addClass('alert-warning');
             }
         });
     });
+    $('#formid').on('click', '#btnupdate', function(event) {
+        event.preventDefault();
+        if($('#formid').valid()){
+            // submit form data to the function save_data() in views.py using ajax
+            // console.log($('#formid').serialize())
+            $.ajax({
+                url: "/update/", 
+                method: "POST",
+                data: $('#formid').serialize(),
+                success: function (data) { 
+                    if(data.status){
+                        x = data.students
+                        output = "";
+                        for(i=0; i<x.length; i++){
+                            output += "<tr id=" + x[i].id + "><td>" + x[i].id + 
+                                    "</td><td>" + x[i].name +
+                                    "</td><td>" + x[i].email +
+                                    "</td><td>" + x[i].phone +
+                                    "</td><td> <input type='button' value='Edit' class='btn btn-warning btn-sm btn-edit' data-sid=" + x[i].id + "> <input type='button' value='Delete' class='btn btn-danger btn-sm btn-delete' data-sid=" + x[i].id +">"
+                        }
+                        $('#tbody').html(output);
+                        $('form')[0].reset();
+
+                        // change update button to save button
+                        $('#btnupdate').off('click');
+                        $('#btnupdate').attr({'value': 'Save', 'id':'btnsave'}).removeClass('btn-warning').addClass('btn-success');
+                        $('#form-title').text('Add Student').removeClass('alert-warning').addClass('alert-info');
+                        // reset the form
+                        $("#formid").validate().resetForm();
+                    }
+                    else{
+                        console.log(data.message)
+                    }
+                 },
+            });
+        }else{
+            console.log('Form is invalid');
+        }
+    });
+    $('#btncancel').click(function () { 
+        // change the button text and attributes
+        $('#btnupdate').off('click');
+        $('#btnupdate').attr({'value': 'Save', 'id':'btnsave'}).removeClass('btn-warning').addClass('btn-success');
+        $('#form-title').text('Add Student').removeClass('alert-warning').addClass('alert-info');
+        $("#formid").validate().resetForm();
+     });
 });
